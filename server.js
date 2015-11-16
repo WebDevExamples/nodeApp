c78364dae5034d8e66fd2ac4e60c4fed7a265e39
wildfireone <wildfireone@gmail.com>
2015-11-16 12:24:35 +0000

    added db info

--- server.js
+++ server.js
@@ -3,7 +3,33 @@
  */
 var http = require('http')
 var port = process.env.PORT || 1337;
+
+
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://main:basiltdog1@ds054308.mongolab.com:54308/rgutest';
+
+
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            res.write('Unable to connect to the mongoDB server. Error:' + err);
+        } else {
+            //HURRAY!! We are connected. :)
+            res.write('Connection established to' + url);
+
+            // do some work here with the database.
+
+            //Close connection
+            db.close();
+        }
+    });
     res.end('Hello World\n');
 }).listen(port);
