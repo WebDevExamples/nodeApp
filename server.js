a0f002653b0092a1caa3da30e842ece89c0b2a38
wildfireone <wildfireone@gmail.com>
2015-11-16 12:28:11 +0000

    added db info

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  */
 var http = require('http');
 //lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
+//var mongodb = require('mongodb');
 var port = process.env.PORT || 1337;
 
 
@@ -20,18 +20,18 @@ http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Connecting');
     // Use connect method to connect to the Server
-    MongoClient.connect(url, function (err, db) {
-        if (err) {
-            res.write('Unable to connect to the mongoDB server. Error:' + err);
-        } else {
-            //HURRAY!! We are connected. :)
-            res.write('Connection established to' + url);
+    //MongoClient.connect(url, function (err, db) {
+    //    if (err) {
+     //       res.write('Unable to connect to the mongoDB server. Error:' + err);
+      //  } else {
+       //     //HURRAY!! We are connected. :)
+        //    res.write('Connection established to' + url);
 
-            // do some work here with the database.
+//            // do some work here with the database.
 
-            //Close connection
-            db.close();
-        }
-    });
+  //          //Close connection
+    //        db.close();
+     //   }
+    //});
     res.end('Hello World\n');
 }).listen(port);
