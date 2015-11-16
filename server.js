0d6b70293b761c1ba98b1d40fcfd9de8caee1a10
wildfireone <wildfireone@gmail.com>
2015-11-16 12:45:56 +0000

    added db info

--- server.js
+++ server.js
@@ -20,18 +20,18 @@ http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Connecting \n');
     // Use connect method to connect to the Server
-    //MongoClient.connect(url, function (err, db) {
-    //    if (err) {
-     //       res.write('Unable to connect to the mongoDB server. Error:' + err);
-      //  } else {
-       //     //HURRAY!! We are connected. :)
-        //    res.write('Connection established to' + url);
+    MongoClient.connect(url, function (err, db) {
+        if (err) {
+            res.write('Unable to connect to the mongoDB server. Error:' + err);
+        } else {
+            //HURRAY!! We are connected. :)
+            res.write('Connection established to' + url);
 
-//            // do some work here with the database.
+            // do some work here with the database.
 
-  //          //Close connection
-    //        db.close();
-     //   }
-    //});
+            //Close connection
+            db.close();
+        }
+    });
     res.end('Hello World\n');
 }).listen(port);
