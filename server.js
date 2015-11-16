de6a62c724e0352ffa85e949fb1c7e7ff29f4ffb
wildfireone <wildfireone@gmail.com>
2015-11-16 12:26:34 +0000

    added db info

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 /**
  * Created by john on 16/11/2015.
  */
-var http = require('http')
+var http = require('http');
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
 var port = process.env.PORT || 1337;
 
 
-//lets require/import the mongodb native drivers.
-var mongodb = require('mongodb');
+
 
 //We need to work with "MongoClient" interface in order to connect to a mongodb server.
 var MongoClient = mongodb.MongoClient;
@@ -17,6 +18,7 @@ var url = 'mongodb://main:basiltdog1@ds054308.mongolab.com:54308/rgutest';
 
 http.createServer(function(req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('Connecting');
     // Use connect method to connect to the Server
     MongoClient.connect(url, function (err, db) {
         if (err) {
