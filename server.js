a47d7b0aba0ade8807208feeb69a6dab182e2de3
wildfireone <wildfireone@gmail.com>
2015-11-23 09:29:57 +0000

    added db info

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ var server = http.createServer(function(request, response) {
     response.write("<title>Hello World Page</title>");
     response.write("</head>");
     response.write("<body>");
+    response.write("<h1>");
     response.write("Hello World!");
+    response.write("</h1>");
     response.write("</body>");
     response.write("</html>");
     response.end();
