2d4ea239f181d7039a78d91830a3243fb909986a
wildfireone <wildfireone@gmail.com>
2015-11-23 09:23:41 +0000

    added db info

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+/**
+ * Created by john on 16/11/2015.
+ */
+var http = require('http');
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+var port = process.env.PORT || 1337;
+
 var http = require('http')
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Hello World\n');
-}).listen(80);
+}).listen(port);
 
