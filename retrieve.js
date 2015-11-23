a0ad9eb4af0b7d35c0d9db034057059b35209d62
wildfireone <wildfireone@gmail.com>
2015-11-23 11:24:47 +0000

    added db info

--- retrieve.js
+++ retrieve.js
@@ -32,7 +32,8 @@ http.createServer(function(request, response) {
             var collection = db.collection('users');
 
             //We have a cursor now with our find criteria
-            var results = collection.find({name: 'modulus user'});
+            var results = collection.find({age: {$lte:30}});
+            //var results = collection.find({name: 'modulus user'});
 
 
             //Lets iterate on the result
