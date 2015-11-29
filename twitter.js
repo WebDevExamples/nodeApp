3a964fc78876e6b442db4ef3c58255a27f84ed4e
Wildfireone <wildfireone@gmail.com>
2015-11-29 19:59:25 +0000

    update response

--- twitter.js
+++ twitter.js
@@ -13,17 +13,15 @@ http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin' : '*'  });
 
     client.get('search/tweets', {q: 'lolcats' , count: '2'}, function(error, tweets){
-        var json = "";
-        json += "{ 'tweets':[ ";
+
+        var json = [];
         for (var i =0; i< tweets.statuses.length ; i++)
         {
-            json += "{ "
-            json += "'name': " + tweets.statuses[i].user.name +",";
-            json += "'text': " + tweets.statuses[i].text;
-            json += " }"
+            json.push({name: tweets.statuses[i].user.name, goals: tweets.statuses[i].text});
         }
-        json +="] }";
-        response.end(json);
+
+
+        response.end(JSON.stringify(json));
     });
 }).listen(port);
 
